4e3f1f5d49eeeb659031da410386e3ca74b988fd
Jerome <hikuluno@gmail.com>
2023-04-03 15:16:27 +0200

    more question in quizz

--- lib/screens/greenlight.dart
+++ lib/screens/greenlight.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-// import 'package:flutter_application_1/score.dart';
+import 'package:flutter_application_1/score.dart';
 
 class GreenLight extends StatefulWidget {
   const GreenLight({Key? key}) : super(key: key);
@@ -10,20 +10,30 @@ class GreenLight extends StatefulWidget {
 }
 
 class _GreenLightState extends State<GreenLight> {
-  // ScoreWidget _scoreWidget = ScoreWidget();
-
+  final GlobalKey<ScoreWidgetState> _scoreKey = GlobalKey();
+  late ScoreWidget _scoreWidget;
   bool _isStarted = false;
+  bool _isWaiting = false;
   late bool _isGreen;
   late bool _isRed;
+
+  @override
+  void initState() {
+    super.initState();
+    _scoreWidget = ScoreWidget(key: _scoreKey);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Green Light'),
-        // actions: [
-        //   _scoreWidget,
-        // ],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: _scoreWidget,
+          ),
+        ],
       ),
       body: !_isStarted
           ? GestureDetector(
@@ -46,43 +56,57 @@ class _GreenLightState extends State<GreenLight> {
                     SizedBox(height: 32),
                     Text(
                       'Swipe left when red',
-                      style: TextStyle(fontSize: 24, color: Colors.white),
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold),
                     ),
                     SizedBox(height: 16),
                     Text(
                       'Swipe right when green',
-                      style: TextStyle(fontSize: 24, color: Colors.white),
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold),
                     ),
                   ],
                 )),
               ),
             )
-          : GestureDetector(
-              onPanUpdate: (details) {
-                int sensitivity = 8;
-                if (details.delta.dx > sensitivity) {
-                  // swipe to the right
-                  if (_isGreen) {
-                    // _scoreWidget.currentState
-                    //     .increment(); // increment the score
-                  }
-                  print("swiped");
-                  _updateColor();
-                } else if (details.delta.dx < sensitivity) {
-                  // swipe to the left
-                  if (_isRed) {
-                    // _scoreWidget.currentState
-                    //     .increment(); // increment the score
-                  }
-                  print("swiped");
-                  _updateColor();
-                }
-              },
-              child: Container(color: _isGreen ? Colors.green : Colors.red)),
+          : _isWaiting
+              ? Container(
+                  color: Theme.of(context).primaryColor,
+                  child: const Center(child: Text("Is waiting")))
+              : GestureDetector(
+                  onPanUpdate: (details) {
+                    int sensitivity = 12;
+                    print("iswaiting : $_isWaiting");
+                    if (details.delta.dx > sensitivity && !_isWaiting) {
+                      // swipe to the right
+                      if (_isGreen) {
+                        setState(() {
+                          _scoreKey.currentState
+                              ?.increment(); // increment the score
+                        });
+                      }
+                      _updateColor();
+                    } else if (details.delta.dx < sensitivity) {
+                      // swipe to the left
+                      if (_isRed) {
+                        setState(() {
+                          _scoreKey.currentState
+                              ?.increment(); // increment the score
+                        });
+                      }
+                      _updateColor();
+                    }
+                  },
+                  child:
+                      Container(color: _isGreen ? Colors.green : Colors.red)),
     );
   }
 
-  void _updateColor() {
+  Future<void> _updateColor() async {
     setState(() {
       _isGreen = Random().nextBool();
       _isRed = !_isGreen;
